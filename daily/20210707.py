a0c319eb43c5ad2e4e7632dd7551c14325098590
rachmatmaulana <maularachmat@gmail.com>
2021-07-08 20:40:48 +0700

    daily 20210707

--- daily/20210707.py
+++ daily/20210707.py
@@ -1,3 +1,27 @@
+# Given a linked list and a number k, rotate the linked list by k places.
+
+# class Node:
+#   def __init__(self, value, next=None):
+#     self.value = value
+#     self.next = next
+
+#   def __str__(self):
+#     current = self
+#     ret = ''
+#     while current:
+#       ret += str(current.value)
+#       current = current.next
+#     return ret
+
+# def rotate_list(list, k):
+#   # Fill this in.
+
+# # Order is 1, 2, 3, 4
+# llist = Node(1, Node(2, Node(3, Node(4))))
+
+# # Order should now be 3, 4, 1, 2
+# print(rotate_list(llist, 2))
+# # 3412
 class Node:
   def __init__(self, value, next=None):
     self.value = value
@@ -12,11 +36,24 @@ class Node:
     return ret
 
 def rotate_list(list, k):
-  # Fill this in.
+  if k==0:
+    return list
+  
+  present = list
 
-# Order is 1, 2, 3, 4
+  while (present.next != None):
+    present = present.next
+  
+  present.next = list
+  present = list
+  
+  for i in range(k-1):
+    present = present.next
+
+  list = present.next
+  present.next = None
+  return list
+
 llist = Node(1, Node(2, Node(3, Node(4))))
 
-# Order should now be 3, 4, 1, 2
-print(rotate_list(llist, 2))
-# 3412
+print(rotate_list(llist,3))
